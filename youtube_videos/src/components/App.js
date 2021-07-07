514ac84fc708b1b5e82a1c20a9f19478105450f5
Jari-Pekka Teurajärvi <teurajarvi@hotmail.com>
2021-07-07 12:28:34 +0300

    Added component SearchBar

--- youtube_videos/src/components/App.js
+++ youtube_videos/src/components/App.js
@@ -1,8 +1,13 @@
 import React from "react";
+import SeachBar from "./SeachBar";
 
 class App extends React.Component {
   render() {
-    return <div>App</div>;
+    return (
+      <div>
+        <SeachBar />
+      </div>
+    );
   }
 }
 
---
+++ youtube_videos/src/components/SeachBar.js
@@ -0,0 +1,9 @@
+import React from "react";
+
+class SeachBar extends React.Component {
+  render() {
+    return <div>SeachBar</div>;
+  }
+}
+
+export default SeachBar;
