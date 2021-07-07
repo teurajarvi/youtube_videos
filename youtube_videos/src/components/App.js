a1fcc11332da7fb5f2b43372c645b828ec018467
Jari-Pekka Teurajärvi <teurajarvi@hotmail.com>
2021-07-07 14:18:58 +0300

    Added onInputChange to search input and onFormSubmit to the form

--- youtube_videos/src/components/App.js
+++ youtube_videos/src/components/App.js
@@ -4,7 +4,7 @@ import SeachBar from "./SeachBar";
 class App extends React.Component {
   render() {
     return (
-      <div>
+      <div className='ui container'>
         <SeachBar />
       </div>
     );
