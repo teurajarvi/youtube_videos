c60bca752a6f1d862b73e9602257c96ef9ef9fef
Jari-Pekka Teurajärvi <teurajarvi@hotmail.com>
2021-07-23 15:31:13 +0300

    Updated default video name

--- youtube_videos/src/components/App.js
+++ youtube_videos/src/components/App.js
@@ -8,7 +8,7 @@ class App extends React.Component {
   state = { videos: [], selectedVideo: null };
 
   componentDidMount() {
-    this.onTermSubmit("Teurastaja");
+    this.onTermSubmit("Jari-Pekka Teurajärvi");
   }
 
   onTermSubmit = async (term) => {
